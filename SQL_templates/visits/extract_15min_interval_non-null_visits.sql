abef3ef1794aff61a500bc8a37cf29bae2f8541d
Godfrey Leung <godfrey.leung@collinsongroup.com>
2026-05-12 16:09:00 +0100

    feat: add scripts to get aggregated 15min interval visits

---
+++ SQL_templates/visits/extract_15min_interval_non-null_visits.sql
@@ -0,0 +1,43 @@
+-- ============================================================================
+-- 15-Minute Interval Visit Count Extraction Query (Past 3 Months)
+-- ============================================================================
+-- Purpose: Count visits per 15-min interval and outlet
+-- Date Range: {start_datetime} to {end_datetime} (exclusive)
+-- Output: Time series of 15-min visit counts with outlet metadata (non-null visits only)
+-- ============================================================================
+
+WITH deduplicate_visits AS (
+    SELECT
+        VISIT_ID,
+        OUTLET_ID AS outlet_code,
+        EXPERIENCE_DATE AS visit_start,
+--        SUM(MEMBER_COUNT + GUEST_COUNT) AS total_visits,
+        MEMBER_COUNT,
+        GUEST_COUNT
+    FROM STAGING_QUALITY.PPASS.SQ_PPASS__CONSOLIDATED_VISITS
+    WHERE EXPERIENCE_DATE >= '{start_datetime}'::timestamp
+      AND EXPERIENCE_DATE < '{end_datetime}'::timestamp
+      AND (
+          experience_date < '2023-06-01'::date
+          OR payment_status <> 'NA'
+      )
+    GROUP BY VISIT_ID, OUTLET_ID, EXPERIENCE_DATE, MEMBER_COUNT, GUEST_COUNT
+),
+visit AS (
+    SELECT
+        outlet_code,
+        visit_start,
+        (MEMBER_COUNT + GUEST_COUNT) AS total_visits,
+        VISIT_ID
+    FROM deduplicate_visits
+)
+SELECT
+    outlet_code,
+    TIME_SLICE(
+        visit_start, 15, 'MINUTE'
+    ) AS visit_slot,
+    COALESCE(SUM(total_visits), 0) AS total_visits,
+    COUNT(DISTINCT VISIT_ID) AS visit_ref_count
+FROM visit
+GROUP BY outlet_code, visit_slot
+ORDER BY visit_slot, outlet_code
---
+++ analysis/scripts/extract/get_15min_visits_outlet_level_via_snowflake.py
@@ -0,0 +1,223 @@
+import datetime as dt
+import logging
+import os
+from pathlib import Path
+import time
+from typing import List
+import warnings
+
+import pandas as pd
+
+import snowflake.connector  # For local run only
+import yaml
+
+# (TODO) NOTE: need to set python path environment in order to import from parent directory
+# from exc import InvalidParameters
+# from helpers import SnowflakeManager
+
+
+# only for local run when storing the extracted data to local
+directory = Path(__file__).parent.parent.parent.parent
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+warnings.filterwarnings("ignore")
+
+
+
+def update_sql_for_extracting_15min_visits(
+    sql_filepath: str, start: str, end: str,
+) -> pd.core.frame.DataFrame:
+    """
+    Updating the SQL template for extracting visits
+     from Snowflake PPass tracking visit table
+
+    Parameters
+    ----------
+    sql_filepath
+        path to the SQL template
+    start
+        start datetime of the period of interest
+    end
+        end datetime of the period of interest
+
+    Returns
+    -------
+        15-min interval visit counts (non-null/zero only) per outlet
+        in the given period of interest
+
+    """
+
+    with open(sql_filepath) as f:
+        sql_query = f.read()
+
+    sql_query = sql_query.replace("{start_datetime}", start)
+    sql_query = sql_query.replace("{end_datetime}", end)
+
+    return sql_query
+
+
+def fetch_15min_visit(
+    data_dir: Path, code_dir: Path
+) -> Path:
+    """
+    Extract the daily visits by lounge and inventory type for
+    a selected list of countries
+
+    Parameters
+    ----------
+    data_dir
+        local (EC2) directory path to export the extracted data to
+    code_dir
+        local directory where the config files are stored
+
+    Returns
+    -------
+        file paths where the extracted visits & corresponding deduced
+        active airports and site counts are exported to
+
+    """
+
+    try:
+        start_time = time.time()
+
+        logger.info(
+            "Starting the ETL job of extracting 15-min visits per outlets ..."
+        )
+
+        # Load parent main config
+        with open(code_dir / "config/get_outlet_visit.yaml", "r") as f:
+            config = yaml.safe_load(f)
+
+        path_config = config["paths"]
+        sql_template = directory / path_config["sql_template"]["get_15min_visit"]
+
+        start_date = config["start_date"]
+        start_date_str = start_date
+        end_date = config["end_date"]
+
+        logger.info(
+            "Connecting to Snowflake and executing the queries for counting 15min visit per outlets ..."
+        )
+
+        # # For local run only. Comment this out and use the following connector if running on ML platform
+        snow_conn = snowflake.connector.connect(
+            user=os.environ["USER"],
+            account=os.environ["SNOWFLAKE_ACCOUNT"],
+            authenticator="externalbrowser",
+            warehouse=os.environ["WAREHOUSE"],
+            database=os.environ["DATABASE"],
+        )
+        cur = snow_conn.cursor()
+
+        # For ML platform run
+        # snowflake_credentials = json.loads(os.environ.get("SNOWFLAKE_CREDENTIALS"))
+        # environment = os.environ.get("ENVIRONMENT")
+        # project_name = os.environ.get("PROJECT_NAME")
+        # stage = os.environ.get("STAGE")
+        # snowflake_manager = SnowflakeManager(
+        #     project_name=project_name,
+        #     stage=stage,
+        #     secret_name=snowflake_credentials[environment]["secret_name"],
+        #     account=snowflake_credentials[environment]["account_name"],
+        #     username_key=snowflake_credentials[environment]["secret_user_key"],
+        #     # password_key=snowflake_credentials[environment]['secret_password_key'],
+        #     privatekey_key=snowflake_credentials[environment]["secret_privatekey_key"],
+        #     passphrase_key=snowflake_credentials[environment]["secret_passphrase_key"],
+        #     warehouse_key=snowflake_credentials[environment]["secret_warehouse_key"],
+        #     environment=environment,
+        #     lambda_function_name=snowflake_credentials[environment].get(
+        #         "snowflake_lambda_name", None
+        #     ),
+        #     aws_access_key_id=snowflake_credentials[environment].get(
+        #         "AWS_ACCESS_KEY_ID", None
+        #     ),
+        #     aws_secret_access_key=snowflake_credentials[environment].get(
+        #         "AWS_SECRET_ACCESS_KEY", None
+        #     ),
+        #     aws_session_token=snowflake_credentials[environment].get(
+        #         "AWS_SESSION_TOKEN", None
+        #     ),
+        #     prod_lambda=snowflake_credentials[environment].get("prod_lambda", "False"),
+        # )
+        # cur = snowflake_manager.get_custom_cursor()
+
+        filename_base = config["filename_base"]
+        export_dir = path_config["export_directory"]
+        output_parent_dir = data_dir / f"{export_dir}"
+
+        intermediate_end_date = dt.date.fromisoformat(start_date)
+        days_to_run = (dt.date.fromisoformat(end_date) - dt.date.fromisoformat(start_date)).days
+
+        full_visit_df = pd.DataFrame()
+        for _ in range(days_to_run):
+            intermediate_end_date = intermediate_end_date + dt.timedelta(days=1)
+            if intermediate_end_date > dt.date.fromisoformat(end_date):
+                break
+
+            intermediate_end_date_str = intermediate_end_date.strftime("%Y-%m-%d")
+            temp_end_date = intermediate_end_date_str
+
+            logger.info(
+                "Extracting 15-min visits per outlets from {} to {} ...".format(start_date, temp_end_date)
+            )
+
+            final_sql_query = update_sql_for_extracting_15min_visits(
+                str(sql_template), start_date, temp_end_date
+            )
+
+            cur.execute(final_sql_query)
+            temp_visit_df = cur.fetch_pandas_all()
+
+            full_visit_df = pd.concat([full_visit_df, temp_visit_df])
+
+            start_date = intermediate_end_date_str
+
+        full_visit_df.sort_values(by=["OUTLET_CODE", "VISIT_SLOT"], inplace=True)
+        full_visit_df = full_visit_df.reset_index(drop=True)
+
+        print(full_visit_df.head())
+        print(full_visit_df.info())
+        print(full_visit_df.nunique())
+
+        input("Check the extracted visit data above. Press Enter to continue ...")
+
+        logger.info("Exporting extracted visits ...")
+
+        filename = f"{filename_base}_by_outlet_{start_date_str}_to_{end_date}.csv"
+        output_file_path = str(output_parent_dir / f"{filename}")
+
+        # export the results to local
+        full_visit_df.to_csv(
+            # the file is saved in the EC2 instance of the SageMaker used for the processing
+            output_file_path,
+            index=False,
+        )
+
+        cur.close()
+
+        end_time = time.time()
+
+        logger.info(
+            "Finished extracting 15-min interval non-null/zero visits per outlet."
+        )
+        logger.info(f"Time used = {end_time - start_time} seconds.")
+
+        return output_file_path
+
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        raise e
+
+
+if __name__ == "__main__":
+
+    visit_output_filepath = (
+        fetch_15min_visit(
+            directory / "data", directory / "analysis"
+        )
+    )
+
+    print(visit_output_filepath)
